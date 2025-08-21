53003a360dcf1af2db22a4d16e8eb26bf796aabc
João Fernando Fernandes Frota Lima <fernandojoao072@gmail.com>
2025-08-21 19:00:38 +0000

    nao esta aparecendo essa logo <iframe src="https://assets.pinterest.com/

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -15,7 +15,7 @@ export default function DashboardLayout({
         <SidebarHeader className="p-4">
           <div className="flex items-center gap-2">
             <Image 
-              src="https://placehold.co/40x40.png" 
+              src="/logo.png" 
               alt="EstateFlow Logo"
               width={40}
               height={40}
