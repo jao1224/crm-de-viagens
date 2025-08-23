3fdc245a910ab79a3a418917a6d4f6a158e5b6e1
João Fernando Fernandes Frota Lima <fernandojoao072@gmail.com>
2025-08-23 11:31:16 +0000

    aplique

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,5 +1,4 @@
 
-
 'use client';
 
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
@@ -310,3 +309,5 @@ export default function DashboardPage() {
     </div>
   );
 }
+
+    
