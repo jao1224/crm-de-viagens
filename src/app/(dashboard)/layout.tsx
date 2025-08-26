b98ac1708bfde93eb69d6e96c3c79417d865c090
Ítalo Lima <italolimac01@gmail.com>
2025-08-26 19:53:54 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <DashboardHeader>

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -11,19 +11,21 @@ export default function DashboardLayout({
 }) {
   return (
       <SidebarProvider>
-          <div className="flex min-h-screen w-full">
+        <div className="flex min-h-screen w-full flex-col">
+          <DashboardHeader />
+          <div className="flex flex-1">
               <Sidebar variant="sidebar" collapsible="icon" className="border-r">
                   <SidebarContent className="p-0">
                       <SimpleDashboardNav />
                   </SidebarContent>
               </Sidebar>
               <div className="flex-1 flex flex-col bg-muted/30">
-                  <DashboardHeader />
                   <main className="flex-1 p-4 md:p-6 relative">
                       {children}
                   </main>
               </div>
           </div>
+        </div>
           <ChatWidget />
       </SidebarProvider>
   );
