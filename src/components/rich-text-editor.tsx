f035ac66215bd4e384c6e01c96ae6f75dc69f984
Ítalo Lima <italolimac01@gmail.com>
2025-08-29 11:25:28 +0000

    esta faltando ainda

--- src/components/rich-text-editor.tsx
+++ src/components/rich-text-editor.tsx
@@ -3,23 +3,35 @@
 
 import { useEditor, EditorContent } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
+import Underline from '@tiptap/extension-underline';
+import TextAlign from '@tiptap/extension-text-align';
 import {
   Bold,
   Italic,
   Strikethrough,
-  Underline,
+  Underline as UnderlineIcon,
   Heading2,
   List,
   ListOrdered,
   Undo,
   Redo,
+  AlignLeft,
+  AlignCenter,
+  AlignRight,
+  AlignJustify,
 } from 'lucide-react';
 import { Toggle } from '@/components/ui/toggle';
 import { Separator } from './ui/separator';
 
 const RichTextEditor = ({ value, onChange }: { value: string; onChange: (value: string) => void }) => {
   const editor = useEditor({
-    extensions: [StarterKit],
+    extensions: [
+        StarterKit, 
+        Underline,
+        TextAlign.configure({
+            types: ['heading', 'paragraph'],
+        }),
+    ],
     content: value,
     onUpdate: ({ editor }) => {
       onChange(editor.getHTML());
@@ -27,7 +39,7 @@ const RichTextEditor = ({ value, onChange }: { value: string; onChange: (value: 
     editorProps: {
       attributes: {
         class:
-          'prose dark:prose-invert prose-sm sm:prose-base lg:prose-lg xl:prose-2xl m-5 focus:outline-none min-h-[150px] border border-input p-2 rounded-md',
+          'prose dark:prose-invert prose-sm sm:prose-base lg:prose-lg xl:prose-2xl mx-auto focus:outline-none min-h-[150px] border border-input p-2 rounded-b-md',
       },
     },
   });
@@ -38,7 +50,22 @@ const RichTextEditor = ({ value, onChange }: { value: string; onChange: (value: 
 
   return (
     <div>
-      <div className="flex flex-wrap items-center gap-1 border border-input p-2 rounded-t-md">
+      <div className="flex flex-wrap items-center gap-1 border border-input p-1 rounded-t-md">
+        <Toggle
+          size="sm"
+          onPressedChange={() => editor.chain().focus().undo().run()}
+          disabled={!editor.can().undo()}
+        >
+          <Undo className="h-4 w-4" />
+        </Toggle>
+        <Toggle
+          size="sm"
+          onPressedChange={() => editor.chain().focus().redo().run()}
+          disabled={!editor.can().redo()}
+        >
+          <Redo className="h-4 w-4" />
+        </Toggle>
+        <Separator orientation="vertical" className="h-6" />
         <Toggle
           size="sm"
           pressed={editor.isActive('bold')}
@@ -58,7 +85,7 @@ const RichTextEditor = ({ value, onChange }: { value: string; onChange: (value: 
           pressed={editor.isActive('underline')}
           onPressedChange={() => editor.chain().focus().toggleUnderline().run()}
         >
-           <Underline className="h-4 w-4" />
+           <UnderlineIcon className="h-4 w-4" />
         </Toggle>
         <Toggle
           size="sm"
@@ -78,6 +105,35 @@ const RichTextEditor = ({ value, onChange }: { value: string; onChange: (value: 
         <Separator orientation="vertical" className="h-6" />
         <Toggle
           size="sm"
+          pressed={editor.isActive({ textAlign: 'left' })}
+          onPressedChange={() => editor.chain().focus().setTextAlign('left').run()}
+        >
+          <AlignLeft className="h-4 w-4" />
+        </Toggle>
+        <Toggle
+          size="sm"
+          pressed={editor.isActive({ textAlign: 'center' })}
+          onPressedChange={() => editor.chain().focus().setTextAlign('center').run()}
+        >
+          <AlignCenter className="h-4 w-4" />
+        </Toggle>
+        <Toggle
+          size="sm"
+          pressed={editor.isActive({ textAlign: 'right' })}
+          onPressedChange={() => editor.chain().focus().setTextAlign('right').run()}
+        >
+          <AlignRight className="h-4 w-4" />
+        </Toggle>
+         <Toggle
+          size="sm"
+          pressed={editor.isActive({ textAlign: 'justify' })}
+          onPressedChange={() => editor.chain().focus().setTextAlign('justify').run()}
+        >
+          <AlignJustify className="h-4 w-4" />
+        </Toggle>
+        <Separator orientation="vertical" className="h-6" />
+        <Toggle
+          size="sm"
           pressed={editor.isActive('bulletList')}
           onPressedChange={() => editor.chain().focus().toggleBulletList().run()}
         >
@@ -89,21 +145,6 @@ const RichTextEditor = ({ value, onChange }: { value: string; onChange: (value: 
           onPressedChange={() => editor.chain().focus().toggleOrderedList().run()}
         >
           <ListOrdered className="h-4 w-4" />
-        </Toggle>
-        <Separator orientation="vertical" className="h-6" />
-         <Toggle
-          size="sm"
-          onPressedChange={() => editor.chain().focus().undo().run()}
-          disabled={!editor.can().undo()}
-        >
-          <Undo className="h-4 w-4" />
-        </Toggle>
-        <Toggle
-          size="sm"
-          onPressedChange={() => editor.chain().focus().redo().run()}
-          disabled={!editor.can().redo()}
-        >
-          <Redo className="h-4 w-4" />
         </Toggle>
       </div>
       <EditorContent editor={editor} />
