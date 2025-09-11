54bc1d9508ddae57b19a422934c9aeb6e1d93fd7
Ítalo Lima <italolimac01@gmail.com>
2025-09-11 11:22:15 +0000

    abra um modal com as opções de filtro (_for element <Button>_)

--- src/app/(dashboard)/pessoa/page.tsx
+++ src/app/(dashboard)/pessoa/page.tsx
@@ -797,7 +797,7 @@ const FilterDialog = ({ open, onOpenChange }: { open: boolean, onOpenChange: (op
             <DialogContent className="sm:max-w-md">
                 <DialogHeader className="flex-row items-center justify-between">
                     <DialogTitle className="text-xl font-bold text-foreground">Filtros</DialogTitle>
-                    <button onClick={() => onOpenChange(false)} className="rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
+                     <button onClick={() => onOpenChange(false)} className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
                         <X className="h-4 w-4" />
                         <span className="sr-only">Fechar</span>
                     </button>
